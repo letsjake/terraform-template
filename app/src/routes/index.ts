0d2f85441c672a5bb3f444f9850e3d8a5dc9b4d1
letsjake <jake@verse2.io>
2024-08-05 15:39:21 +0900

    feat: update workflow

--- app/src/routes/index.ts
+++ app/src/routes/index.ts
@@ -8,7 +8,7 @@ const router = Router();
 router.get('/', (req, res) => {
     try {
         const serverIp = ip();
-        res.status(200).send(`Hello, World! Server IP: ${serverIp}`);
+        res.status(200).send(`bye, World! Server IP: ${serverIp}`);
     } catch (error) {
         errorHandler(error, res);
     }
